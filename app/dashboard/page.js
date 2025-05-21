c3f3ec8628106c9f3339c8e10b34ee9e05aeeeb7
Anjali Bartwal <anjalibartwal@pearlorganisation.com>
2025-05-21 12:54:32 +0530

    dsvbszdf

--- app/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p>Welcome to the Admin Dashboard!</p>
-    </div>
-  );
-}
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import { ReduxProvider } from "@/lib/redux/provider";
 import "./globals.css";
+import { WanderTripSidebar } from "@/component/sidebar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <ReduxProvider>
+        <WanderTripSidebar />
          {children}
         </ReduxProvider>
       </body>
